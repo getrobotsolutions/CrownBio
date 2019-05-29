dad53f67ce121366d3e675f492e0fd1840df0997
tamoorkayani <tamoorkayani@gmail.com>
2019-05-29 22:32:39 +0500

    Slotmachine and Badge scanning update

--- Contents/Selfie/Scripts/PhotoPrinting.js
+++ Contents/Selfie/Scripts/PhotoPrinting.js
@@ -225,7 +225,7 @@ function SendEmail()
     var tempEmail=message;
     //var addr = "paul@robotaisolutions.com";
     var subject = "Your robot selfie";
-    var body = "Thank you for visiting Blase Manufacturing! Your robot selfie is attached.";
+    var body = "Thank you for visiting CrownBio at ASCO. Please find attached your picture. Find out more at https://www.crownbio.com/resources";
     //var mail_server = "smtp.gmail.com";
     //var mail_addr_sender = "ict.muscatairport@gmail.com";
     //var pswd = "123456789K";
@@ -257,7 +257,7 @@ function SendEmail()
         data: { 
           
            'email':tempEmail,
-           'apID':'Passco'
+           'apID':'CrownBio'
         }
       }).done(function(response) {
         console.log('saved: ' + response); 
